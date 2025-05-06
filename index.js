f9d0b614c11ea66aba0d8b2e7575cd99ad4e2d0c
Drazen Drinic <drazendrinic@hotmail.com>
2025-05-06 10:26:59 +0200

    Add & Style: card

--- index.js
+++ index.js
@@ -21,3 +21,33 @@ buttonEngEl.addEventListener('click', () => {
   buttonLatEl.classList.remove('btn-active');
   buttonCyrEl.classList.remove('btn-active');
 });
+
+const cardOneEl = document.getElementById('card-one');
+const cardOneFrontCardEL = document.querySelector(
+  '#card-one .card-outer.front'
+);
+const cardOneBackCardEL = document.querySelector('#card-one .card-outer.back');
+
+let isReveald = false;
+
+cardOneEl.addEventListener('click', function () {
+  if (!isReveald) {
+    isReveald = true;
+
+    cardOneFrontCardEL.style.transformOrigin = 'bottom left';
+    cardOneFrontCardEL.style.transform = 'translateX(-140px) rotate(-5deg)';
+    cardOneFrontCardEL.style.transition = 'transform 0.5s ease';
+
+    cardOneBackCardEL.style.transformOrigin = 'bottom right';
+    cardOneBackCardEL.style.transform = 'translateX(140px) rotate(5deg)';
+    cardOneBackCardEL.style.transition = 'transform 0.5s ease';
+  } else {
+    isReveald = false;
+
+    cardOneFrontCardEL.style.transform = 'translateX(0px) rotate(0deg)';
+    cardOneFrontCardEL.style.transition = 'transform 0.5s ease';
+
+    cardOneBackCardEL.style.transform = 'translateX(0px) rotate(0deg)';
+    cardOneBackCardEL.style.transition = 'transform 0.5s ease';
+  }
+});
